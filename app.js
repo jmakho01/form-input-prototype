d911792bea6fe8763bcb1038f0ec5600e6cfe94e
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2025-10-21 16:08:10 -0700

    Port update

--- app.js
+++ app.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost.$(PORT)`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
