bf4a67a6aa327c63e81f9144b80a38ee73f9f024
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-10-22 19:12:51 -0700

    Added rough input fields and input validation to page

--- public/scripts/cardLoader.js
+++ public/scripts/cardLoader.js
@@ -45,6 +45,7 @@ class cardForOthersToUse extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
+    this.isEditing = false; // edit flag
   }
 
   // Use a single, asynchronous connectedCallback
@@ -88,8 +89,8 @@ class cardForOthersToUse extends HTMLElement {
       const wrapper = document.createElement('form')
       wrapper.setAttribute("method=\"post\"")
       wrapper.setAttribute("action=\"/submit-order\"")
-    } */
-   /*else {*/
+    }
+    else { */
     const wrapper = document.createElement('div');
     //}
     // Do not add the CSSStyleSheet object directly as a class
@@ -106,18 +107,64 @@ class cardForOthersToUse extends HTMLElement {
             ${Object.entries(courses).map(([course, teacher]) => `
               <tr>
                 <td class="CourseCard">${course}</td>
-                <td class="TeacherCard">${teacher}</td>
+                <td class="TeacherCard">${this.isEditing && !readOnly ? `<input type="text" value="${teacher}" data-subject="${subject}" data-course="${course}">` : teacher }</td>
               </tr>
             `).join('')}
           </tbody>
         </table>
       `).join('')}
       <br>
-      <input style="width: 100px; height: 40px; justify-self: center;" type="button" name="Edit" value="Edit" onClick='alert("User prompted to edit")'>
+      <div class="button-row">
+        <input style="width: 100px; height: 40px; justify-self: center;" type="button" class="editbtn" value="${this.isEditing && !readOnly ? "Save" : "Edit"}"'>
+        ${this.isEditing && !readOnly ? `<input style="width: 100px; height: 40px; justify-self: center;" type="button" class="cancelbtn" value="Cancel">` : ''}
+      </div>
     `;
 
     this.shadowRoot.innerHTML = '';
     this.shadowRoot.appendChild(wrapper);
+
+    const editButton = this.shadowRoot.querySelector('.editbtn');
+    editButton.addEventListener('click', () => {
+      if (this.isEditing && !readOnly) {
+        this.saveChanges();
+      } else {
+        this.tempData = JSON.parse(JSON.stringify(this._data)); // stores backup of JSON data
+      }
+      this.isEditing = !this.isEditing;
+      this.updateCard();
+    });
+
+    const cancelButton = this.shadowRoot.querySelector('.cancelbtn');
+    if (cancelButton) {
+      cancelButton.addEventListener('click', () => {
+        if (this.tempData) {
+          this._data = JSON.parse(JSON.stringify(this.tempData)); // restore backup of JSON data
+          this.tempData = null;
+        }
+        this.isEditing = false;
+        this.updateCard();
+      });
+    }
+  }
+
+  // function for saving changes done in form
+  saveChanges() {
+    const inputs = this.shadowRoot.querySelectorAll('input[type="text"]');
+
+    // pulls in data from input
+    inputs.forEach(input => {
+      const subject = input.dataset.subject;
+      const course = input.dataset.course;
+      const newValue = input.value.trim();
+
+      if (newValue === "") { // if input field is blank...
+        alert("Please fill in all fields before saving."); // displays error message
+        return; // reverts to non-saved data
+      }
+      else {
+        this._data.tables[subject][course] = newValue;
+      }
+    });
   }
 }
 
