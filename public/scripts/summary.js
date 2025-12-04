2606712cfb6b62b637f755b8178d3ddabc3d3631
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2025-12-03 23:21:46 -0800

    Added additional logic to filter years in summary page

--- public/scripts/summary.js
+++ public/scripts/summary.js
@@ -1,6 +1,21 @@
-let table = new DataTable('#loc-table');
-$("#reviewFilter").on("change", function() {
-    table.column(0)  // Under Review column index
-    .search(this.value)
-    .draw();
+document.addEventListener("DOMContentLoaded", function () {
+    const table = $("#loc-table").DataTable({
+        order: [] // prevents automatic initial sorting
+    });
+
+    // review filter
+    $("#reviewFilter").on("change", function () {
+        table.column(1).search(this.value).draw();
+    });
+
+    // year filter
+    $("#yearFilter").on("change", function () {
+        let year = this.value;
+
+        if (year === "") {
+            table.column(0).search("").draw();
+        } else {
+            table.column(0).search("^" + year + "$", true, false).draw();
+        }
+    });
 });
